c18366976d178eab069997ce10e2dee5aadcd663
Tiago Martins Vargas <vargas.tiago@hotmail.com>
2022-05-31 12:35:33 -0300

    Start project and added assets folder

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import { styles } from './styles'
+
+
+
+export function SignIn() {
+  return(
+    <View style={styles.container}>
+      <Text>Hello My Friend</Text>
+    </View>
+  )
+}
